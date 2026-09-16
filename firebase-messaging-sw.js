51a326d27c3fb1b2ddf37f48aff145c0abc6f69a
mk18renrakucho <moguler3@gmail.com>
2026-09-17 02:29:09 +0900

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDHTAfFAKYy5qBE8fGx7tq7nY2X1dOGjG0",
+  authDomain: "katsuchu-82b50.firebaseapp.com",
+  projectId: "katsuchu-82b50",
+  storageBucket: "katsuchu-82b50.firebasestorage.app",
+  messagingSenderId: "933868129628",
+  appId: "1:933868129628:web:493881226f832777e00599"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('裏側で通知を受け取りました: ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: './icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
